41d3e3510bb5037c6f1c864f4c854b37eae44b0b
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2024-08-12 08:05:04 +0100

    Basic of how to play a hand

--- xmas_bash_v1/xmas.go
+++ xmas_bash_v1/xmas.go
@@ -1,1 +1,30 @@
-package xmas
+package xmas_bash_v1
+
+type Option int
+
+const (
+	Rock Option = iota
+	Paper
+	Scissors
+)
+
+type HandResult int
+
+const (
+	Tie HandResult = iota
+)
+
+type Game struct {
+}
+
+func (game *Game) FirstPlayerSelect(option Option) {
+
+}
+
+func (game *Game) SecondPlayerSelect(option Option) {
+
+}
+
+func (game *Game) Result() HandResult {
+	return Tie
+}
--- xmas_bash_v1/xmas_test.go
+++ xmas_bash_v1/xmas_test.go
@@ -1,1 +1,17 @@
-package xmas_test
+package xmas_bash_v1_test
+
+import (
+	"testing"
+	xmas "xmas-bash/xmas_bash_v1"
+)
+
+func TestFullGame(t *testing.T) {
+	game := xmas.Game{}
+
+	game.FirstPlayerSelect(xmas.Rock)
+	game.SecondPlayerSelect(xmas.Rock)
+	expected := xmas.Tie
+	if game.Result() != expected {
+		t.Errorf("Result: Expected %d but got %d ", expected, game.Result())
+	}
+}
